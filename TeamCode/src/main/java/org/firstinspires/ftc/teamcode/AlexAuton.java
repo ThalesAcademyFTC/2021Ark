d3ddbd7ded84816096da69377dc46850c54c9d6c
evan-blip <evan.wieberg@students.thalesacademy.org>
2020-10-22 16:16:22 -0400

    Added AlexAuton base code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexAuton.java
@@ -1,67 +1,51 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
+
+//================================================================================================\\
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+//================================================================================================\\
+
 import static org.firstinspires.ftc.teamcode.Anvil.Drivetrain.EVAN;
 import static org.firstinspires.ftc.teamcode.Anvil.Drivetrain.MECHANUM;
 
+//================================================================================================\\
 
 @Autonomous(name="AlexAuton", group="Pushbot")
 @Disabled
+
+//================================================================================================\\
 
 public class AlexAuton extends LinearOpMode {
     private Anvil robot;
     private ElapsedTime runtime = new ElapsedTime();
     @Override
     public void runOpMode() {
+
+//================================================================================================\\
+
         robot = new Anvil(hardwareMap, MECHANUM, telemetry);
         telemetry.addData("Status", "Initialized");
         runtime.reset();
         telemetry.update();
         waitForStart();
-        robot.moveForTicks(100);
 
+//================================================================================================\\
 
+        robot.moveLeft(1);
+        sleep(1);
+        robot.moveBackward(1);
+        sleep(1);
+        robot.moveRight(1);
+        sleep(1);
+        robot.moveForward(1);
 
+//================================================================================================\\
 
-        while (opModeIsActive() && runtime.milliseconds() < 30000) {
-
-
-
+        while (opModeIsActive() && runtime.milliseconds() < 2) {
         }
-
-
     }
 }
