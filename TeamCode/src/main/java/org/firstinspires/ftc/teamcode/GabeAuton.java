aeb334053aff717244d00b17201cbf0ee84addf8
FTCTeam11424 <35707077+FTCTeam11424@users.noreply.github.com>
2020-12-03 16:02:48 -0500

    Finished up GabeAuton, ready to input code for different positions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GabeAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GabeAuton.java
@@ -54,17 +54,16 @@ public class GabeAuton extends LinearOpMode {
     private static final String LABEL_FIRST_ELEMENT = "Quad";
     private static final String LABEL_SECOND_ELEMENT = "Single";
     private Anvil robot;
-    private WTR wtr;
     private ElapsedTime runtime = new ElapsedTime();
     private static final String VUFORIA_KEY =
             "AXeANof/////AAABmaDs6FJl000DqMZb1GO2za8OTVHuQt7JKLF1cPYd4n6ToTb5uU19/BWc39F/bsNlzfVOalKd3tJdD3X42NYGDc/4PIfKaeWQG2Km+Ge8ueZcxkflgpLg/jjvxYAcJ7me0MM+j/5pJX1WQQLQCjrb2eXn8oCARcCsQeVq4VA0QtJOBvaE6Bvz3T8LS8c1CiJlb64sC5cS55824oq9RjtsYeWhbfh1uOV+hVg74AdwCu3VZC8CFJQNRKNL0ZPALv6PZWclrppxVESZF+aSYX5SIZln6C1iwvQskiJV2T4Xy45OP5PmO0vGW8P2r86+yG6y046AOeECsrLtLqonxWIpYXjnHvWpNgbarU/vPQgQET6i";
     private VuforiaLocalizer vuforia;
     private TFObjectDetector tfod;
+    private String lastRecognized;
 
     @Override
     public void runOpMode() {
         robot = new Anvil(hardwareMap, UNNAMED, telemetry);
-        wtr = new WTR();
         telemetry.addData("Status", "Initialized");
 
         initVuforia();
@@ -79,7 +78,39 @@ public class GabeAuton extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
+        // Checks for disks
+        if (opModeIsActive()) {
+            while (opModeIsActive() && runtime.milliseconds() < 7000) {
+                // telemetry.addData("redSensor", robot.sensorColor.red());
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        // step through the list of recognitions and display boundary info.
+                        int i = 0;
+                        for (Recognition recognition : updatedRecognitions) {
+                            lastRecognized = recognition.getLabel();
+                        }
+                    }
+                }
+            }
+            if (tfod != null) {
+                tfod.shutdown();
+            }
 
+        }
+        if (lastRecognized == "Quad"){
+            //Position C
+            telemetry.addData("Target", "C");
+        } else if (lastRecognized == "Single"){
+            //Position B
+            telemetry.addData("Target", "B");
+
+        } else {
+            //Position A
+            telemetry.addData("Target", "A");
+        }
       /*  while (robot.sensorColor.red() < 110){
             robot.moveForward(0.4);
             telemetry.addData("redSensor", robot.sensorColor.red());
@@ -88,33 +119,9 @@ public class GabeAuton extends LinearOpMode {
         while (robot.sensorColor.red() < 110 && robot.sensorColor.blue() < 110 )
         robot.rest(); */
 
-        if (opModeIsActive()) {
-            while (opModeIsActive() && runtime.milliseconds() < 30000) {
-                telemetry.addData("redSensor", robot.sensorColor.red());
-                if (tfod != null) {
-                    // getUpdatedRecognitions() will return null if no new information is available since
-                    // the last time that call was made.
-                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                    if (updatedRecognitions != null) {
-                        telemetry.addData("# Object Detected", updatedRecognitions.size());
-                        // step through the list of recognitions and display boundary info.
-                        int i = 0;
-                        for (Recognition recognition : updatedRecognitions) {
-                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                    recognition.getLeft(), recognition.getTop());
-                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                    recognition.getRight(), recognition.getBottom());
-                        }
-                        telemetry.update();
-                    }
-                }
-
-            }
-            if (tfod != null) {
-                tfod.shutdown();
-            }
-
+        while (opModeIsActive() && runtime.milliseconds() < 30000) {
+            telemetry.addData("lastRecognized", lastRecognized);
+            telemetry.update();
         }
     }
     private void initVuforia() {
